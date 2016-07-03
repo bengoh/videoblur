0561b23b46d3996b1d3c7abab10076d800aff2bc
Benjamin Goh <masotime@gmail.com>
2016-07-02 23:17:59 -0700

    Added on-the-fly Babel transpilation

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-var express = require('express');
-
+const express = require('express');
+const expressBabel = require('express-babel').default;
 const app = express();
+
+app.use('/js', 
+	expressBabel(__dirname + '/public/js', { presets: [ 'es2015', 'stage-0' ] })
+);
 
 app.use(express.static('public'));
 
